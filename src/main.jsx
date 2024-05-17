e93ac52740181f819d27b87b5d8f68c833820329
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-05-17 14:42:26 +0100

    Follow along tutorial up to the end of 'global pending UI' section

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import ErrorPage from './error-page';
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from './routes/root';
+import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
+
 import './index.css';
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: '/contacts/:contactId',
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: '/contacts/:contactId/edit',
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Setup complete!</h1>
+    <RouterProvider router={routes} />
   </React.StrictMode>,
 );
