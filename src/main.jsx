4d8f22011bd1bac4c1cd3cefa4854d688577b0c7
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-05-28 09:33:07 +0100

    Clear out files for another runthrough of the tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -1,61 +1,9 @@
+import ReactDOM from 'react-dom';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import ErrorPage from './error-page';
-import Root, {
-  loader as rootLoader,
-  action as rootAction,
-} from './routes/root';
-import Index from './routes/index';
-import Contact, {
-  loader as contactLoader,
-  action as contactAction,
-} from './routes/contact';
-import EditContact, { action as editAction } from './routes/edit';
-import { action as destroyAction } from './routes/destroy';
-
 import './index.css';
-
-const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: true,
-            element: <Index />,
-          },
-          {
-            path: '/contacts/:contactId',
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction,
-          },
-          {
-            path: '/contacts/:contactId/edit',
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-          {
-            path: '/contacts/:contactId/destroy',
-            action: destroyAction,
-          },
-        ],
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <h1>React Router Tutorial</h1>
   </React.StrictMode>,
 );
