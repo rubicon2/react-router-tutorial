58088352e8d82a2c4e27e31d6be4367dacd5d205
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-05-28 12:22:05 +0100

    Complete tutorial up to end of cancel button

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,13 @@ import Root, {
   loader as rootLoader,
   action as rootAction,
 } from './routes/root';
+import Index from './routes/index';
 import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact, {
+  loader as editLoader,
+  action as editAction,
+} from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
 
 const routes = [
   {
@@ -15,12 +21,23 @@ const routes = [
     children: [
       {
         index: true,
-        element: <h2>Wow, I am index child</h2>,
+        element: <Index />,
       },
       {
         path: '/contacts/:contactId',
         element: <Contact />,
         loader: contactLoader,
+      },
+      {
+        path: '/contacts/:contactId/edit',
+        element: <EditContact />,
+        loader: editLoader,
+        action: editAction,
+      },
+      {
+        path: '/contacts/:contactId/destroy',
+        action: destroyAction,
+        errorElement: <div>Something went wrong!</div>,
       },
     ],
   },
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,11 @@
-import { Link, Outlet, Form, useLoaderData } from 'react-router-dom';
+import {
+  NavLink,
+  Outlet,
+  Form,
+  useLoaderData,
+  useNavigation,
+  redirect,
+} from 'react-router-dom';
 import { getContacts, createContact } from '../contacts';
 
 export async function loader() {
@@ -8,11 +15,12 @@ export async function loader() {
 
 export async function action() {
   const contact = await createContact();
-  return { contact };
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
 
   return (
     <>
@@ -38,19 +46,27 @@ export default function Root() {
           <ul>
             {contacts.map((contact) => (
               <li key={contact.id}>
-                <Link to={`/contacts/${contact.id}`}>
+                <NavLink
+                  to={`/contacts/${contact.id}`}
+                  className={({ isActive, isPending }) =>
+                    isActive ? 'active' : isPending ? 'pending' : ''
+                  }
+                >
                   {contact.first || contact.last ? (
                     contact?.first + ' ' + contact?.last
                   ) : (
                     <i>No Name</i>
                   )}
-                </Link>
+                </NavLink>
               </li>
             ))}
           </ul>
         </nav>
       </div>
-      <div id="detail">
+      <div
+        id="detail"
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
         <Outlet />
       </div>
     </>
